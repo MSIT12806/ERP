0d4354d4e92476a2e03ae3b9be2c4a372e16c499
Ron <alegalone@gmail.com>
2023-04-06 15:28:06 +0800

    列車增刪修查

--- TrainSystem/Domain/interfaces/ITrainPersistant.cs
+++ TrainSystem/Domain/interfaces/ITrainPersistant.cs
@@ -3,5 +3,11 @@
     public interface ITrainPersistant
     {
         TrainData GetTrain(string trainID);
+        IEnumerable<TrainData> GetTrains(DateTime startTime,DateTime endTime);
+        IEnumerable<TrainData> GetTrainsByID(string trainID, DateTime dateTime);
+        IEnumerable<TrainData> GetTrainsByStation(string stationName, DateTime dateTime);
+        TrainData AddTrain(string trainID);
+        TrainData RemoveTrain(string trainID);
+        TrainData EditTrain(string trainID);
     }
 }
